919eab27a90a584e7f70e51b1e668e0974aa3a41
afplc-iscteiul <afplc@iscte-iul.pt>
2019-11-13 14:15:53 +0000

    Menu, IG

--- Projecto ES/src/Menu.java
+++ Projecto ES/src/Menu.java
@@ -17,79 +17,91 @@ import javax.swing.WindowConstants;
 public class Menu {
 	private JFrame frame;
 
-	public Menu(String title) {
-		frame = new JFrame(title);
-		
+	public Menu() {
+		frame = new JFrame("Software");
+
 		// para que o botao de fechar a janela termine a aplicacao
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
+
 		addFrameContent();
-		
-		// para que a janela se redimensione de forma a ter todo o seu conteudo visivel
+
+		// para que a janela se redimensione de forma a ter todo o seu conteudo
+		// visivel
 		frame.pack();
 	}
 
-	public void open(int width, int height) {
-		frame.setSize(width, height);
-		
+	public void open() {
+		 frame.setSize(300, 250);
+
 		// para abrir a janela (torna-la visivel)
 		frame.setVisible(true);
 	}
 
 	private void addFrameContent() {
-		
-		
-		/* para organizar o conteudo em grelha (linhas x colunas)
-		se um dos valores for zero, o numero de linhas ou colunas (respetivamente) fica indefinido,
-		e estas sao acrescentadas automaticamente */
-		frame.setLayout(new GridLayout(4,0));
 
-		JLabel label = new JLabel("Title");
-		frame.add(label);
+		/*
+		 * para organizar o conteudo em grelha (linhas x colunas) se um dos
+		 * valores for zero, o numero de linhas ou colunas (respetivamente) fica
+		 * indefinido, e estas sao acrescentadas automaticamente
+		 */
+		frame.setLayout(new GridLayout(5,0));
 
-		JTextField text = new JTextField("Hello");
-		frame.add(text);
-		
-		JLabel widthLabel = new JLabel("width");
-		frame.add(widthLabel);
-
-		JTextField widthText = new JTextField("300");
-		frame.add(widthText);
-		
-		JLabel heightLabel = new JLabel("height");
-		frame.add(heightLabel);
-
-		JTextField heightText = new JTextField("150");
-		frame.add(heightText);
-		
-		JCheckBox center = new JCheckBox("center");
-		
-		JButton update = new JButton("update");
-		update.addActionListener(new ActionListener() {
+		JButton selecionar = new JButton("Selecionar ficheiro");
+		selecionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Alterar titulo
-				frame.setTitle(text.getText());
-				
-				//Alterar tamanho
-				frame.setSize(Integer.parseInt(widthText.getText()), Integer.parseInt(heightText.getText()));
-				if(center.isSelected()) {
-					centerFrame();
-				}
+				// açao-- JOANA MOUTINHO
 			}
 		});
-		frame.add(update);	
-		frame.add(center);
+		frame.add(selecionar);
 		
+		
+
+		JButton visualizar = new JButton("Visualizar Ficheiro");
+		visualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// açao -- carolina
+			}
+
+		});
+		frame.add(visualizar);
+		
+		JButton editar_criar = new JButton("Editar/Criar Regras");
+		editar_criar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// açao-- 
+			}
+
+		});
+		frame.add(editar_criar);
+		
+		JButton detetar = new JButton("Detetar defeitos");
+		detetar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// açao
+			}
+
+		});
+		frame.add(detetar);
+		
+		JButton analisar = new JButton("Analisar qualidade");
+		analisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// açao
+			}
+
+		});
+		frame.add(analisar);
 	}
 
 	private void centerFrame() {
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		
+
 		frame.setLocation(dimension.width / 2 - frame.getWidth() / 2, dimension.height / 2 - frame.getHeight() / 2);
-		
+
 	}
+
 	public static void main(String[] args) {
-		Menu window = new Menu("Hello");
-		window.open(300,150);
+		Menu window = new Menu();
+		window.open();
 	}
 }
